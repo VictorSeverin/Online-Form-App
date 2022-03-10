fc2ca22a047a56b1eed7295e5d1c78e6384aa57a
victor severin <vikea.severin@gmail.com>
2022-03-10 15:28:22 -0800

    created navbar component

--- team-bravo/src/App.js
+++ team-bravo/src/App.js
@@ -1,24 +1,9 @@
 import logo from './logo.png';
-import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"  />
-        <p>
-          Project is on the way
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-          Team Bravo
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
