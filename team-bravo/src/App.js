b15dd01bd0d8ab798505621f16ca2c565f102ec1
Christopher Chafin <chafin@Christophers-MacBook-Air.local>
2022-03-14 10:17:22 -0700

    add maintable component

--- team-bravo/src/App.js
+++ team-bravo/src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.png';
 import './App.css';
+import MainTable from'./components/MainTable'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"  />
-        <p>
-          Project is on the way
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-          Team Bravo
-        </a>
-      </header>
+      <MainTable />
     </div>
   );
 }
---
+++ team-bravo/src/components/MainTable.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function MainTable() {
+    return(
+        <>
+            <h1>Welcome</h1>
+            <h2>to Team Bravo's Web Form Generator</h2>
+        </>
+    )
+}
+
+export default MainTable;
