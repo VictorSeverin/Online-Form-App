375dc486324b44db58a3e15d70b4ca234d36a7ad
Christopher Chafin <chafin@Christophers-MacBook-Air.local>
2022-03-15 00:01:56 -0700

    add empty shape in main page

--- team-bravo/src/components/MainTable.js
+++ team-bravo/src/components/MainTable.js
@@ -1,10 +1,20 @@
 import React from "react"
 
+function EmptyTable() {
+    return(
+        <div className="emptytable--shape">
+            <img src="assets/circle-plus-solid.png" className="emptytable--icon"/>
+            <p className="emptytable--text">Please add an element</p>
+        </div>
+    )
+}
+
 function MainTable() {
     return(
         <>
-            <h1>Welcome</h1>
-            <h2>to Team Bravo's Web Form Generator</h2>
+            <h1 className="maintable--title">Welcome</h1>
+            <h2 className="maintable--subtitle">to Team Bravo's Web Form Generator</h2>
+            <EmptyTable />
         </>
     )
 }
