8b12670fc46014a225ef84a8934f4f7e21deae27
Christopher Chafin <chafin@Christophers-MacBook-Air.local>
2022-03-15 17:06:25 -0700

    add maintable to app.js

--- team-bravo/src/App.js
+++ team-bravo/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.png';
 import './App.css';
-import MainTable from'./components/MainTable'
+import MainTable from'./components//MainTable/MainTable'
 
 function App() {
   return (
