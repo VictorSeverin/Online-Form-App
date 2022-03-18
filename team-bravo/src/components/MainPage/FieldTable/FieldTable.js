f111768df01388f440406cc6914e9e517feedb3b
Christopher Chafin <chafin@Christophers-MacBook-Air.local>
2022-03-17 19:20:38 -0700

    create a basic table with headers

--- team-bravo/src/components/MainPage/FieldTable/FieldTable.js
+++ team-bravo/src/components/MainPage/FieldTable/FieldTable.js
@@ -1,13 +1,27 @@
 import React from "react"
 
 class FieldTable extends React.Component {
+    testIndex = 0 // this is just for the dummy data
+
     constructor(props) {
         super(props)
+        
+        // for now create a local array of all the fields
+        this.state = { fields: [] }
     }
 
     render() {
         return (
-            <div>Add table here</div>
+            <div className="fieldtable">
+                <table>
+                    <thead>
+                        <th>Title</th>
+                        <th>Key</th>
+                        <th>Type</th>
+                        <th>Required</th>
+                    </thead>
+                </table>
+            </div>
         )
     }
 }
