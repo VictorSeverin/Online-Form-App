6c2985f7a16104a627e0bec7663ed748f1047dce
Christopher Chafin <chafin@Christophers-MacBook-Air.local>
2022-03-17 19:14:52 -0700

    renamed MainTable to MainPage

--- team-bravo/src/App.js
+++ team-bravo/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.png';
 import './App.css';
-import MainTable from'./components//MainTable/MainTable'
+import MainPage from'./components/MainPage/MainPage'
 
 function App() {
   return (
     <div className="App">
-      <MainTable />
+      <MainPage />
     </div>
   );
 }
--- team-bravo/src/components/MainPage/MainPage.js
+++ team-bravo/src/components/MainPage/MainPage.js
@@ -1,23 +1,24 @@
 import React from "react";
-import './MainTable.css';
+import './MainPage.css';
 
+/*
 function EmptyTable() {
     return(
         <div className="emptytable--shape">
             <img src="assets/circle-plus-solid.png" className="emptytable--icon"/>
-            <p className="emptytable--text">Please add an element</p>
+            <p className="emptytable--text">Please add an Element</p>
         </div>
     )
 }
+*/
 
-function MainTable() {
+function MainPage() {
     return(
-        <div className="maintable">
+        <div className="mainpage">
             <h1>Welcome</h1>
             <h2>to Team Bravo's Web Form Generator</h2>
-            <EmptyTable />
         </div>
     )
 }
 
-export default MainTable;
+export default MainPage;
