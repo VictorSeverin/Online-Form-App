f008df88ea4c4b9c30ff5f581b6ecf69de645c3f
VictorSeverin <vikea.severin@gmail.com>
2022-04-12 11:30:35 -0700

    added form title and description inputs

--- team-bravo/src/components/MainPage/MainPage.js
+++ team-bravo/src/components/MainPage/MainPage.js
@@ -2,22 +2,35 @@ import React from "react";
 import FieldTable from "./FieldTable/FieldTable"
 import * as IoIcons from "react-icons/io";
 import {useState} from "react"
+import { Form } from 'react-bootstrap'
 function MainPage({data}) {
-    const [elems,setElems] = useState(data)
-    const handleClick = () =>{
-        console.log(data.lenght);
+    const [elems,setElems] = useState([data])
+    const [formTitle, setFormTitle] = useState()
+    const [formDesc, setFormDesc] = useState()
+    const handleClick = (data) =>{
     }
     return(
         <div className="mainpage">
-            <h1>Welcome</h1>
-            <h2>to Team Bravo's Web Form Generator</h2>
+            <h2 onClick={handleClick}>Team Bravo's Web Form Generator</h2>
+            <div className="form-info">
+                <Form> 
+                    <Form.Group className="mb-3 form-info-input" controlId="formBasicEmail">
+                        <Form.Label>Form Title</Form.Label>
+                        <Form.Control type="text" size="lg" placeholder="Enter Title" onChange={(e) => setFormTitle(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3 form-info-ta" controlId="ControlTextarea1">
+                        <Form.Label>Form Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder="Enter Description" onChange={(e) => setFormDesc(e.target.value)}/>
+                    </Form.Group>
+                </Form>
+            </div>
             <div className="table-wrapper">
                 <div className="table-component">
-                    {data ? (
+                    {!data.lenght ? (
                         <FieldTable data={data}/>
                     ) : (
                     <div className="optional-message"> 
-                        <IoIcons.IoMdAddCircleOutline className="table-svg" onClick={handleClick}/>
+                        <IoIcons.IoMdAddCircleOutline className="table-svg" />
                         <p>No Elements to show</p>
                     </div>
                     )}
