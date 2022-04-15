0a70650f3368a45ba21c7d5d0ea1a27d387b0d31
Christopher Chafin <chafin@christophersair.lan>
2022-04-14 23:14:45 -0700

    rewrapped the context provider since the route needs access to the finalelems as well

--- team-bravo/src/App.js
+++ team-bravo/src/App.js
@@ -14,69 +14,39 @@ import FinalForm from './components/Form/FinalForm'
 import FieldContextProvider from './components/Context/FieldContext';
 function App() {
   const [title,setTitle] = useState('');
-  const [finalElems,setFinalElems] = useState([])
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = (item) => {
     setShow(true);
     setTitle(item)
   }
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setFinalElems(tasksFromServer)
-    }
 
-    getTasks()
-  }, [])
-
-  // Fetch Tasks
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:8080/api/v1/types')
-    const data = await res.json()
-
-    console.log(data)
-    return data
-  }
-
-  const addElem = (elem) => {
-    setShow(false)
-    setFinalElems([...finalElems, elem])
-    console.log(elem)
-    
-    service.createType(elem).then((response) =>{
-      console.log(response.data)
-    }).then(error =>{
-      console.log(error)
-    })
-  }
   return (
-    <Router>
-      <div className="App">
-        {/* <Sidebar addElement={addElement} /> */}
-        <Routes>
-          <Route 
-          path="/"
-          exact 
-          element={           
-            <>
-              <FieldContextProvider>
+    <FieldContextProvider>
+      <Router>
+        <div className="App">
+          {/* <Sidebar addElement={addElement} /> */}
+          <Routes>
+            <Route 
+            path="/"
+            exact 
+            element={           
+              <>
                 <Sidebar handleShow={handleShow} />
-                <MainPage data={finalElems} />
+                <MainPage />
                 <ModalComp 
                   handleClose={handleClose}
                   handleShow={handleShow}
                   show={show}
-                  onAdd={addElem}
                   title={title} />
-              </FieldContextProvider>
-            </>
-            } />
-            <Route path='/form' element={<FinalForm data={finalElems} />} />
+              </>
+              } />
+              <Route path='/form' element={<FinalForm data={finalElems} />} />
 
-          </Routes>
-      </div>
-    </Router>
+            </Routes>
+        </div>
+      </Router>
+    </FieldContextProvider>
   );
 }
 
