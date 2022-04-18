88f006f94f9d91cec58342df4a1408311a2ec4a1
Christopher Chafin <chafin@Christophers-MacBook-Air-TEST.local>
2022-04-18 13:44:45 -0700

    dynamically added edit button for edit modal

--- team-bravo/src/components/MainPage/FieldTable/EditField.js
+++ team-bravo/src/components/MainPage/FieldTable/EditField.js
@@ -11,6 +11,49 @@ function EditField({element, index}) {
     const [placeholder, setPlaceholder] = useState(element.placeholder)
     const [required, setRequired] = useState(element.required)
 
+    const [showPlaceholder, setShowPlaceholder] = useState(() => {
+        switch (title) {
+            case 'Input':
+                return true
+                break
+            case 'Textarea':
+                return true
+                break
+            case 'Check Box':
+                return false
+                break
+            case 'Radio Button':
+                return false
+                break
+            case 'Select Menu':
+                return false
+                break
+            case 'Email':
+                return true
+                break
+            case 'Email Confirm':
+                return true
+                break
+            case 'Number':
+                return true
+                break
+            case 'Password':
+                return true
+                break
+            case 'Password Confirm':
+                return true
+                break
+            case 'Currency':
+                return true
+                break
+            case 'Toggle':
+                return false
+                break
+            default:
+                break
+        }
+    })
+
     const updatedField = {title, label, placeholder, required}
 
     const onSubmit = (e) => {
@@ -25,10 +68,18 @@ function EditField({element, index}) {
                 <Form.Label>Label</Form.Label>
                 <Form.Control 
                     type="text" 
-                    placeholder="Enter Label" 
+                    placeholder="Enter label" 
                     value={label}
                     onChange={(e) => setLabel(e.target.value)} />
             </Form.Group>
+            {showPlaceholder && <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Placeholder</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter placeholder"
+                        value={placeholder}
+                        onChange={(e) => setPlaceholder(e.target.value)} />
+                </Form.Group>}
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check 
                     type="checkbox" 
